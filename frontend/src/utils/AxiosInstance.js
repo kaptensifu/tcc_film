8a755b885f2730076ba0a388473d084cf5aa9010
Kanang Herdaya <kanang107@gmail.com>
2025-05-25 12:11:16 +0700

    instance

--- frontend/src/utils/AxiosInstance.js
+++ frontend/src/utils/AxiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://takhir-film-948060519163.us-central1.run.app/films",
   withCredentials: true,
 });
 
